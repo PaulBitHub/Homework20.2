18d4d662b267094e8c4e0c0749cf7962cae2ec3b
Павел Сергеевич <p.bitiakov.official@gmail.com>
2024-09-25 17:39:56 +0300

    добавление связи с views

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import home, contact
+from catalog.views import home, contact, base
 
 
 app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name="home"),
     path("contacts/", contact, name="contact"),
+    path("base/", base, name="base"),
 ]
